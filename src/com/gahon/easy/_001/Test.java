5957ca420644fa3fbe8df7890018d93c05041efc
Gahon <1053475617@qq.com>
2018-01-16 18:42:16 +0800

    Finish the Test

--- src/com/gahon/easy/_001/Test.java
+++ src/com/gahon/easy/_001/Test.java
@@ -2,6 +2,6 @@ package com.gahon.easy._001;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello World");
+        System.out.println("This is the Test File");
     }
 }

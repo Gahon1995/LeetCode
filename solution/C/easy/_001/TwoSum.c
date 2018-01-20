fe2e8acdbdf000261a9760fcc72f3a9fb8cbe7b6
Gahon <1053475617@qq.com>
2018-01-20 21:44:02 +0800

    C solve TwoSum

--- solution/C/easy/_001/TwoSum.c
+++ solution/C/easy/_001/TwoSum.c
@@ -1,5 +1,16 @@
-#include <stdio.h>
-
-int main(){
-    printf("hello")
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target) {
+    int * results =(int *)malloc(sizeof(int)*2);
+    for(int i=0;i<numsSize-1;++i){
+        for (int j=i+1;j<numsSize;++j){
+            if(nums[i]+nums[j]==target){
+                results[0]=i;
+                results[1]=j;
+            }
+        }
+    }
+    return results;
 }
+

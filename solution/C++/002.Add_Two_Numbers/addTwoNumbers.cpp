b692ac9d312ba661444215329a134e92261d217b
Gahon <1053475617@qq.com>
2018-01-22 17:07:51 +0800

    add addTwoNumbers.cpp

--- solution/C++/002.Add_Two_Numbers/addTwoNumbers.cpp
+++ solution/C++/002.Add_Two_Numbers/addTwoNumbers.cpp
@@ -12,31 +12,13 @@ public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int carry = 0;
         ListNode *res = new ListNode(0),*newres =res;
-        ListNode *longList;
-        while((l1!=NULL)&&l2!=NULL){
-        	ListNode *tmp = new ListNode(0);
-        	carry = l1->val+l2->val+carry;
-        	tmp->val = carry%10;
+        while(l1||l2||carry){
+        	carry = (l1?l1->val:0)+(l2?l2->val:0)+carry;
+        	newres->next = new ListNode(carry%10);
         	carry = carry/10;
-        	l1 = l1->next;
-        	l2 = l2->next;
-
-        	newres->next = tmp;
-        	newres = tmp;
-        }
-        longList = (l1!=NULL)?l1:l2;
-        while(longList!=NULL){
-        	ListNode *tmp = new ListNode(0);
-        	carry = longList->val+carry;
-        	tmp->val = carry%10;
-        	carry = carry/10;
-        	longList=longList->next;
-        	newres->next = tmp;
-        	newres = tmp;
-        }
-        if(carry==1){
-        	ListNode *tmp = new ListNode(1);
-        	newres->next = tmp;
+        	newres=newres->next;
+        	l1 = l1?l1->next:NULL;
+        	l2 = l2?l2->next:NULL;
         }
         return res->next;
     }
